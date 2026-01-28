c6fb9182d517caa12cb6d8bdd8c88d4807e58df1
grawler5 <123873896+grawler5@users.noreply.github.com>
2026-01-29 00:36:56 +0300

    Replace PNG app icons with SVG

--- Web/public/sw.js
+++ Web/public/sw.js
@@ -4,8 +4,8 @@ const ASSETS = [
   '/',
   '/index.html',
   '/manifest.webmanifest',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icons/icon-192.svg',
+  '/icons/icon-512.svg'
 ];
 
 self.addEventListener('install', (event) => {
